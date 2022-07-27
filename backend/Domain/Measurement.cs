93293258972943d1bc8d71ead15faa6039ea9905
Noah Scharrenberg <nscharrenberg@hotmail.com>
2022-07-27 19:46:15 +0200

    add basic Model structure

---
+++ backend/Domain/Measurement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    internal class Measurement
+    {
+        public int Id { get; }
+        public Station Station { get; private set; }
+        public double Temperature { get; set; }
+        public double FeelTemperature { get; set; }
+        public double GroundTemperature { get; set; }
+        public string WindDirection { get; set; }
+        public int SunPower { get; set; }
+        public double RainFallLastDay { get; set; }
+    }
+}
---
+++ backend/Domain/Station.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    internal class Station
+    {
+        public Station(int stationId, string name, string region)
+        {
+            StationId = stationId;
+            Name = name;
+            Region = region;
+            Measurements = new List<Measurement>();
+        }
+
+        public int Id { get; }
+        public int StationId { get; private set; }
+        public string Name { get; set; }
+        public string Region { get; set; }
+        public List<Measurement> Measurements { get; set; }
+
+    }
+}
